36a91c48b32314a131a39d4454258ee7c3668d93
RaulCesar1 <raulcesardeoliveira2@gmail.com>
2022-12-04 22:24:10 -0300

    1.1.0

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
+import textos from '../utils/textos.json'
 
 const Home: NextPage = () => {
   return (
@@ -13,11 +13,12 @@ const Home: NextPage = () => {
       <main className="">
 
         <div className='flex flex-col text-center'>
-          <h1 className='text-7xl italic text-amber-200 font-bold'>Clube de robótica</h1>
+          <h1 className='text-7xl italic text-amber-400 font-bold'>Clube de robótica</h1>
+          <h2 className='text-4xl my-4 italic text-yellow-400 font-medium'>{textos.clube_sobre}</h2>
           <div className='divPrincipalGlass flex flex-col flex-wrap m-40 w-5/6 rounded-md'>
-            <div className='m-6 rounded-lg'>
+{/*             <div className='m-6 rounded-lg'>
               <a href="sobre"><h1 className='w-full h-40 grid place-content-center text-6xl text-orange-200 font-medium hover:text-8xl'>Sobre</h1></a>
-            </div>
+            </div> */}
             <div className='m-6 rounded-lg'>
               <a href="projetos"><h1 className='w-full h-40 grid place-content-center text-6xl text-orange-200 font-medium hover:text-8xl'>Projetos</h1></a>
             </div>
--- pages/projetos.tsx
+++ pages/projetos.tsx
@@ -3,7 +3,7 @@ import Head from 'next/head'
 
 const Projetos: NextPage = () => {
   return (
-    <div className="bganimado h-screen w-screen antialiased select-none">
+    <div className="bganimado h-screen w-screen antialiased select-none overflow-hidden">
       <Head>
         <title>Robótica Valois</title>
         <link rel="icon" href="/favicon.ico" />
@@ -12,9 +12,22 @@ const Projetos: NextPage = () => {
       <main className="">
 
         <div className='flex flex-col text-center'>
-          <a href="/"><h1 className='text-7xl italic text-amber-200 font-bold hover:text-8xl'>Clube de robótica</h1></a>
+          <a href="/"><h1 className='text-7xl italic text-amber-400 font-bold hover:text-8xl'>Clube de robótica</h1></a>
           <div className=''>
             <h1 className='text-4xl font-medium text-orange-100'>Projetos desenvolvidos:</h1>
+            <div className='grid grid-cols-3 gap-4 m-16'>
+              <img src='/robocasa1.jpg' className='h-60 rounded-lg' />
+              <img src='/robocasa2.jpg' className='h-60 rounded-lg' />
+              <img src='/robopronto1.jpg' className='h-60 rounded-lg' />
+              <img src='/robopronto2.jpg' className='h-60 rounded-lg' />
+              <img src='/fotopessoal1.jpg' className='h-60 rounded-lg' />
+              <video controls className='h-60 rounded-lg'> 
+                <source src='/videoOBR.mp4' type='video/mp4'/>
+              </video>
+              <video controls className='h-60 rounded-lg'> 
+                <source src='/videoPrimeiroTeste.mp4' type='video/mp4'/>
+              </video>
+            </div>
           </div>
         </div>
         
